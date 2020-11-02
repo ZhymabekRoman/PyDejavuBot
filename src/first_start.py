fa6a840b0ee77af13f4879acc3c11408a52a1316
ZhymabekRoman <robanokssamit@yandex.ru>
2020-11-02 09:31:47 +0600

    Add Config Master

--- src/first_start.py
+++ src/first_start.py
@@ -1,10 +1,15 @@
 import sqlite3
 import os.path
+print("Config Master")
 
-db_name = input("Укажите название db: ")
+# Step 1 : database init 
+db_name = input("Укажите название db (default-myTable): ")
+
+if db_name == "":
+	db_name = "myTable"
 
 if os.path.isfile(f"{db_name}.db") is True:
-    print("Файл существует! Отмена ...")
+    print("Файл базы данных существует! Отмена ...")
     exit()
 
 conn = sqlite3.connect(f"{db_name}.db")
@@ -12,4 +17,16 @@ cur = conn.cursor()
 
 cur.execute("CREATE TABLE  IF NOT EXISTS  users(user_id TEXT, lang TEXT, projects TEXT)")
 conn.commit()
+
+# Step 2 :config init
+if os.path.isfile("config.py") is True:
+    print("Файл конфигурации существует! Отмена ...")
+    exit()
+
+tlgrm_bot_api = input("Укажите API токен бота: ")
+
+file = open("config.py", "w")
+file.write(f"API_TOKEN = '{tlgrm_bot_api}'" + '\n')
+file.write(f"database_name = '{db_name}.db'")
+
 print("Готово !!!")
