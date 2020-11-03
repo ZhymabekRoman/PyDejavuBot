7ae66ce521d3ea3bc71e7759cfbd6b8262dcbbd7
ZhymabekRoman <robanokssamit@yandex.ru>
2020-11-03 17:58:41 +0600

    Add threading

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ import re
 import config
 import logging
 import asyncio
+import threading
 from aiogram.utils.exceptions import BotBlocked
 from aiogram.utils.markdown import text, bold, italic, code, pre
 from aiogram import Bot, Dispatcher, executor, types
